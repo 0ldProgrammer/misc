7978ef5625455811fa5be7260eec5c469b996be5
OldProgrammer <53154671+0ldProgrammer@users.noreply.github.com>
2021-05-07 18:40:18 +0200

    Update api_flask.py

--- api_flask.py
+++ api_flask.py
@@ -12,7 +12,6 @@ def get_last_number_of_messages():
 		Retrieve the last file value in the folder and increment the value.
 		__get_last_number_of_messages(k)__.
 	'''
-
 	current_directory_flask_list = []
 
 	for last_message_in_the_directory in os.listdir('messages'):
@@ -26,26 +25,22 @@ def get_last_number_of_messages():
 @app.route('/contact_api_json', methods=["GET", "POST"])
 def contact_api_json():
 	'''
-		You can send confidential data to our server, we will look into it all.
-		return : json page!
+		In the POSTDATA, it should have information like this :
+		{"lastname":"your_last_name", "firstname":"your_first_name", "message":"your_message_to_send"}
 	'''
 	if(flask.request.method == "POST"):
 		flask_request_json   = flask.request.json
 		for item_pull_request in flask_request_json.items():
 			if("lastname" and "firstname" and "message" in item_pull_request):
-				'''
-					In the POSTDATA, it should have information like this :
-					{"lastname":"your_last_name", "firstname":"your_first_name", "message":"your_message_to_send"}
-				'''
 				try:
 					flask_request_json["message"] = eval(flask_request_json["message"])
-
+					
 				except NameError as exception_error_flask_request:
 					flask_request_json["message"] = flask_request_json["message"]
-
+					
 				except SyntaxError as exception_error_flask_request:
 					flask_request_json["message"] = flask_request_json["message"]
-
+					
 				with open("messages/" + get_last_number_of_messages(), "w") as write_new_file:
 					write_new_file.write("\nLastname: %s\nFirstname: %s\nMessage: %s\n\n" %(flask_request_json["lastname"], flask_request_json["firstname"], flask_request_json["message"]))
 					return flask_request_json
